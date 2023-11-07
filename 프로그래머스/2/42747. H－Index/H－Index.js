28f583cecc5ff736bdc3945c1a813a2214fc9124
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-11-07 13:51:51 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,10 @@
+function solution(citations) {
+    let answer = 0;
+    const sort = [...citations].sort((a,b)=> b-a);
+    const {length} = sort
+    for(let i=0; i<length; i++){
+        if(sort[i] > i) answer = i+1
+        else if(sort[i] === i) answer = i
+    }
+    return answer;
+}
