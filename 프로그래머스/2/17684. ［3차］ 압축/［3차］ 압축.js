08c5b6a4e62632d842a48ad0f257efa56c9cfeba
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-11-15 13:25:39 +0900

    [level 2] Title: [3차] 압축, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,53 @@
+const OBJ = {
+    A: 1,
+    B : 2,
+    C : 3,
+    D : 4,
+    E : 5,
+    F : 6,
+    G : 7,
+    H : 8,
+    I : 9,
+    J : 10,
+    K : 11,
+    L : 12,
+    M : 13,
+    N: 14,
+    O: 15,
+    P:16,
+    Q:17,
+    R:18,
+    S: 19,
+    T:20,
+    U:21,
+    V:22,
+    W:23,
+    X:24,
+    Y:25,
+    Z:26,
+}
+
+let num = 26
+
+function solution(msg) {
+    const answer = []
+    let next = 1
+    for(let i=0; i<msg.length; i+=next){
+        next = 1
+        let char = msg[i]
+        let nextChar = char + msg[i+1]
+        while(true){
+            if(OBJ[char] && !OBJ[nextChar]) {
+                answer.push(OBJ[char])
+                num += 1
+                OBJ[nextChar] = num
+                break
+            }
+            if(msg[i+next] === undefined) break
+            char = nextChar
+            next+=1
+            nextChar = nextChar+msg[i+next]
+        }
+    }
+    return answer
+}
