3fda86864359e21ce49249111e235c29d29e6f92
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-01-02 15:14:34 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,6 @@
+function solution(numbers) {
+    const answer = numbers.map(num => String(num)).sort((a,b)=>{
+        return (b+a)-(a+b)
+    }).join('')
+    return answer[0] === "0" ? "0" : answer
+}
