d9f5400e56913f49ae14bf5ad5d6c2d21618b59c
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-01-21 17:25:58 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -1,38 +1,35 @@
-function checkTicTacToe(board, sign) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const [a, b, c] of lines) {
-    if (board[a] == sign && board[b] == sign && board[c] == sign) return true;
-  }
-  return false;
+function solution(board) {
+// 이긴 상황 O -> O가 1개 더 많음 
+// 이긴 상황 X -> X와 O의 개수가 같음 
+// 비긴 상황 혹은 진행 상황 O, X의 개수가 같음 혹은 O의 개수가 1개 더 많음 
+    const newBoard = board.join('').split('')
+    let countX = getCount(newBoard, "X");
+    let countO = getCount(newBoard, "O");
+    
+    if(countO < countX || countO-countX > 1) return 0
+    
+    let winO = checkWin(newBoard, "O");
+    let winX = checkWin(newBoard, "X");
+    
+    if(winO && winX) return 0
+    if(winO > 1 && winX > 1) return 0
+    if(winO  && countO-countX !== 1) return 0
+    if(winX  && countO-countX !== 0) return 0
+    return 1
 }
 
-function solution(board) {
-  board = board.map(val => val.split('')).flat();
-  let [O, X] = [0, 0];
+function getCount(board,base){
+    return board.filter(item => item === base).length
+}
 
-  for (const sign of board) {
-    if (sign === 'O') O++;
-    else if (sign === 'X') X++;
-  }
-
-  if (O < X || 1 < O - X) return 0;
-
-  let oWins = checkTicTacToe(board, 'O');
-  let xWins = checkTicTacToe(board, 'X');
-
-  if (oWins && xWins) return 0;
-  if (oWins && O - X !== 1) return 0;
-  if (xWins && O !== X) return 0;
-
-  return 1;
+function checkWin(board, base){
+    let count = 0;
+    const winArr = [
+        [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+    ]
+    winArr.forEach(win => {
+        const [a,b,c] = win
+        if(board[a] === base && board[b] === base && board[c]===base) count+=1
+    })
+    return count
 }
