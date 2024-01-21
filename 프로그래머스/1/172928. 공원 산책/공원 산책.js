e2b72dd88c961ae8a22c5512a5105828efff41df
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-01-21 16:03:07 +0900

    [level 1] Title: 공원 산책, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,37 +1,45 @@
 function solution(park, routes) {
-    var answer = [];
-    const {length: yLength} = park
-    const {length: xLength} = park[0]
-    let h = 0;
-    let w = 0
-    
-    for(let i=0; i<yLength; i++){
-        for(let j=0; j<xLength; j++){
-            if(park[i][j] === "S"){
-                w = j
-                h = i
-                break
-            }
-        }
+    let [x,y] = findStart(park);
+    const lenX = park.length;
+    const leyY = park[0].length;
+    const direction = {
+        "E" : [0,1],
+        "W" : [0,-1],
+        "S" : [1,0],
+        "N" : [-1,0]
     }
-    const direction = {E: [0, 1], W: [0, -1], S: [1, 0], N: [-1, 0] }
     
-    for(let i=0; i<routes.length; i++){
-        const [type, length] = routes[i].split(' ')
-        let [nx,ny] = [w,h]
+    routes.forEach(route=> {
+        const [type, len] = route.split(' ');
+        let nx = x;
+        let ny = y;
         let cnt = 0
-        while(cnt<length){
-            nx +=direction[type][1]
-            ny +=direction[type][0]
-            if(nx >= xLength || nx <0 || ny>=yLength || ny <0 || park[ny][nx] === "X"){
-                break
-            }
-            cnt +=1
+        while(cnt < Number(len)){
+            nx += direction[type][0];
+            ny += direction[type][1];
+            if(nx<0 || nx>=lenX || ny<0 || ny>=leyY || park[nx][ny] === "X") break;
+            cnt += 1;
         }
-        if(cnt === Number(length)){
-            w = nx
-            h = ny
+        if(cnt === Number(len)){
+            x= nx;
+            y= ny;
+        }
+    })
+    return [x,y]
+}
+
+
+function findStart(park) {
+    let x = 0;
+    let y = 0;
+    for(let i=0; i<park.length; i++){
+        for(let j=0; j<park[0].length; j++){
+            if(park[i][j] === "S"){
+                x = i;
+                y = j;
+                break;
+            }
         }
     }
-    return [h,w]
+    return [x,y];
 }
