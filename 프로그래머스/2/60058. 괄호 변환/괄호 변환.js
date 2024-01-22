e8ac83a2423add31585c3db3905964b8bd611c26
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-01-22 13:18:51 +0900

    [level 2] Title: 괄호 변환, Time: 3.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -0,0 +1,22 @@
+function solution(p) {
+    if(p === "") return ""
+    
+    let [u,v] = ["",""]
+    let [a,b] = [0,0]
+    
+    for(let i=0; i<p.length; i++){
+        p[i] === "(" ? a+=1 : b+=1;
+        u += p[i]
+        if(a===b){
+            v=p.slice(i+1, p.length)
+            break
+        }   
+    }
+    console.log(u,v)
+    if(u[u.length-1] === ")") return u+solution(v)
+    else{
+        u= u.slice(1,u.length-1).split('')
+        for (let i = 0; i < u.length; i += 1) u[i] = u[i] === `)` ? `(` : `)`
+        return `(${solution(v)})`+u.join('')
+    }
+}
