affc9346c8624700ce4e309f678c76615fa4a5f5
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-01 16:07:51 +0900

    [level 2] Title: n^2 배열 자르기, Time: 585.66 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,8 +1,11 @@
 function solution(n, left, right) {
-    const answer = [];
+    const answer = []
+    const arr = Array.from({length:n}, (_,idx)=>idx+1);
+
     for(let i=left; i<=right; i++){
-      const value = Math.max(Math.floor(i/n), i%n);
-      answer.push(value+1)
+        const value = Math.floor(i/n);
+        const paste = i%n
+        value >= paste? answer.push(arr[value]) : answer.push(arr[paste])
     }
     return answer
 }
