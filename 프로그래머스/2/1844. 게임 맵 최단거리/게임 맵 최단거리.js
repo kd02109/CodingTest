475badd9ad26204dd381b1b4787db00e6470cdd3
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-04 17:15:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.72 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,27 +1,29 @@
 function solution(maps) {
-  const [xLength, yLength] = [maps.length, maps[0].length];    
-  const visited = Array.from({length: xLength}, ()=>new Array(yLength).fill(false))
-  const [lastX, lastY] =  [xLength-1, yLength-1]
-  let answer = 1;
-  const dx = [0,1,0,-1];
-  const dy = [1,0,-1,0];
-  const queue = [[0,0,1]];
-  visited[0][0] = false;
+    const destination = [maps.length-1, maps[0].length-1]
+    const dx = [-1,0,1,0];
+    const dy = [0,1,0,-1];
+    let answer = 0;
+    const queue = [[0,0,1]];
+    maps[0][0] = 0;
+    
+    while(queue.length){
+        let [x,y,count] = queue.shift();
+        if(x === destination[0] && y ===destination[1]){
+            answer = count;
+            break;
+        }
+        
+        for(let i=0; i<dx.length; i+=1){
+            let nx = x+dx[i];
+            let ny = y+dy[i];
+            if(nx>=0 && nx<maps.length && ny >=0 && ny<maps[0].length && maps[nx][ny] === 1){
+                maps[nx][ny] = 0;
+                queue.push([nx,ny,count+1]);
+            }
+        }
 
-  while(queue.length>0){
-      const [x,y,count] = queue.shift()
-      if(x === lastX && y === lastY){
-          answer = count
-          break;
-      }
-      for(let i=0; i<4; i++){
-          let nx = x+dx[i];
-          let ny = y+dy[i];
-          if(nx >= 0 && nx < xLength && ny >= 0 && ny < yLength && visited[nx][ny] === false && maps[nx][ny] === 1){
-              visited[nx][ny] = true;
-              queue.push([nx,ny,count+1])
-          }
-      }
-  }  
-    return answer === 1? -1: answer
+    } 
+
+    if(answer === 0) return -1
+    return answer
 }
