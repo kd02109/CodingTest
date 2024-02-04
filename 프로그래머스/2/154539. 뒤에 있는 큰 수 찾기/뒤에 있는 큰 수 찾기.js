583a457b9265f94db37761b02bd3fbf3e0b2707b
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-04 18:20:07 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 130.59 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,18 +1,13 @@
 function solution(numbers) {
-    // 정답을 numbers의 길이만큼 -1로 생성
-    const answer = Array.from({length:numbers.length}, ()=>-1);
-    
-    // stack에는 numbers의 인덱스 값을 넣습니다.
+    const answer = Array.from({length:numbers.length},()=>-1)
     const stack = []
     
-    for(let i = 0; i<numbers.length; i++){
-        while(stack.length>0 && numbers[stack.at(-1)] < numbers[i] ){
-            // 현재 기준의 수가 stack의 마지막 값보다 클 경우, 
-            // 해당 stack 인덱스의 정답을 현재 비교하는 숫자로 교체
-            // stack에서 제거
-            answer[stack.pop()] = numbers[i]
+    for(let i=0; i<numbers.length; i+=1){
+        while(stack.length>0 && numbers[stack[stack.length-1]] < numbers[i]){
+            const idx = stack.pop();
+            answer[idx] = numbers[i];
         }
         stack.push(i)
     }
-    return answer
+    return answer;
 }
