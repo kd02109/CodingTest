a743aed00e9859d8eea14893835e0befaf260c69
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-08 12:07:29 +0900

    [level 2] Title: 숫자 변환하기, Time: 147.40 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,10 +1,10 @@
 function solution(x, y, n) {
-    const cache = new Array(y+1).fill(Infinity);
-    cache[x] = 0
-    for(let i =x ; i<=y ; i++){
-        cache[i+n] = Math.min(cache[i+n], cache[i]+1);
-        cache[i*2] = Math.min(cache[i*2], cache[i]+1);
-        cache[i*3] = Math.min(cache[i*3], cache[i]+1);
+    const cache = Array.from({length:y+1}, ()=>Infinity);
+    cache[x] = 0;
+    for(let i=x; i<y; i+=1){
+        cache[i+n] = Math.min(cache[i]+1, cache[i+n]);
+        cache[i*2] = Math.min(cache[i]+1, cache[i*2]);
+        cache[i*3] = Math.min(cache[i]+1, cache[i*3]);
     }
-    return cache[y] === Infinity ? -1 : cache[y]
+    return cache[y] === Infinity? -1 : cache[y]
 }
