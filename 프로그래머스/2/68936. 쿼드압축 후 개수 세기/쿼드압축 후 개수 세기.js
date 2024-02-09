61123dc2438827e4060d5d593807e12e039f3d21
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-09 14:06:34 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.81 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,28 +1,28 @@
 function solution(arr) {
     const answer = [0,0];
-    
-    function def(row, col, len){
-        let flag = true;
-        
-        for(let i=row; i<row+len; i++){
-            for(let j=col; j<col+len; j++){
-                if(arr[row][col] !== arr[i][j]) {
-                    flag = false;
-                    break;
-                }
-            }
-        }    
-        const half = len / 2;
+   
+    function dfs(row, col, half){
+      let flag = true;
+      const base = arr[row][col]
+      for(let i=row; i<row+half; i+=1){
+          for(let j=col; j<col+half; j+=1){
+              if(arr[i][j] !== base) {
+                  flag = false;
+                  break;
+              }
+          }
+          if(!flag) break;
+      }
+       half /= 2;
         if(!flag){
-            def(row, col, half);
-            def(row+half, col, half);
-            def(row, col+half, half);
-            def(row+half, col+half, half);
+            dfs(row, col, half);
+            dfs(row+half, col, half);
+            dfs(row, col+half, half);
+            dfs(row+half, col+half, half);
         }else{
             arr[row][col] === 1 ? answer[1] += 1 : answer[0] += 1
         }
-        
     }
-    def(0,0,arr.length)
-    return answer;
+    dfs(0,0, arr.length);
+    return answer
 }
