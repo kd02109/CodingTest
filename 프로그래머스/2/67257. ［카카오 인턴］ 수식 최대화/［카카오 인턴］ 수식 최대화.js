e1681961e4f2e10db57936fcefec8c2e03601aea
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-14 12:45:37 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.80 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -1,31 +1,27 @@
 function solution(expression) {
     const priors = [
-        ["-", "+" , "*"],
-        ["-", "*" , "+"],
-        ["+", "-" , "*"],
-        ["+", "*" , "-"],
-        ["*", "-" , "+"],
-        ["*", "+" , "-"],
-    ]    
-
-    let answer = 0;
+        ["+","-","*"],
+        ["+","*","-"],
+        ["-","+","*"],
+        ["-","*","+"],
+        ["*","-","+"],
+        ["*","+","-"],
+    ]
     
+    let answer = 0;
     priors.forEach(prior => {
-        // 핵심
-        let temp = expression.split(/(\D)/)
+        let temp = expression.split(/(\D)/);
         prior.forEach(cal => {
             while(temp.includes(cal)){
-                const idx = temp.indexOf(cal)
-                let num = 0
-                if(cal === "+") num = Number(temp[idx-1]) + Number(temp[idx+1])
-                if(cal === "-") num = Number(temp[idx-1]) - Number(temp[idx+1])
-                if(cal === "*") num = Number(temp[idx-1]) * Number(temp[idx+1])   
-                temp = [...temp.slice(0,idx-1), num, ...temp.slice(idx+2)]
+                const idx = temp.indexOf(cal);
+                let num = 0;
+                if(cal === "+") num = Number(temp[idx-1]) + Number(temp[idx+1]);
+                if(cal === "-") num = Number(temp[idx-1]) - Number(temp[idx+1]);
+                if(cal === "*") num = Number(temp[idx-1]) * Number(temp[idx+1]);
+                temp = [...temp.slice(0,idx-1), num, ...temp.slice(idx+2)];
             }
         })
         answer = Math.max(answer, Math.abs(temp[0]));
     })
- 
-
-    return answer;
+    return answer
 }
