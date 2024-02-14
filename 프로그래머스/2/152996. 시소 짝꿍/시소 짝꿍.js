d79aae03f05014d99cacfa5831134ad34b97253b
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-14 12:39:13 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,16 +1,15 @@
 function solution(weights) {
     const obj = {};
-    const cals = [1,3/2, 2 , 4/3];
+    const cases = [1,2,3/2,4/3];
     let answer = 0;
-    
-    weights.sort((a,b)=>b-a).forEach((weight,idx)=>{
-        cals.forEach(cal => {
-            if(obj[weight*cal]) answer += obj[weight*cal]
+    weights.sort((a,b)=>b-a);
+    weights.forEach(weight=>{
+        cases.forEach(cal => {
+            if(obj[weight*cal]) answer += obj[weight*cal];
         })
         if(obj[weight]) obj[weight] += 1;
-        if(!obj[weight]) obj[weight] = 1;
+        else obj[weight] = 1;
     })
-    
     return answer;
 }
 
