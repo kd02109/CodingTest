b20ab32dd234776fa5ac4183e75a94cf4daf5dd3
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-20 13:33:41 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -1,35 +1,32 @@
 function solution(board) {
-// 이긴 상황 O -> O가 1개 더 많음 
-// 이긴 상황 X -> X와 O의 개수가 같음 
-// 비긴 상황 혹은 진행 상황 O, X의 개수가 같음 혹은 O의 개수가 1개 더 많음 
-    const newBoard = board.join('').split('')
-    let countX = getCount(newBoard, "X");
-    let countO = getCount(newBoard, "O");
+    const boards = board.join('').split('');
+    const O = "O";
+    const X = "X"
     
-    if(countO < countX || countO-countX > 1) return 0
+    function getCounting(symbol){
+        return board.join('').split('').filter(item => item===symbol).length;
+    }
+    const numO = getCounting(O);
+    const numX = getCounting(X);
     
-    let winO = checkWin(newBoard, "O");
-    let winX = checkWin(newBoard, "X");
+    if(numX > numO) return 0;
+    if(numO - numX > 1) return 0;
+   
+    function checkWin (symbol){
+        let possible = 0;
+        const wins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+        wins.forEach(win => {
+            if(boards[win[0]] === symbol && boards[win[1]] === symbol && boards[win[2]] === symbol) possible +=1;
+        })
+        return possible;
+    }
+    
+    const winO = checkWin(O);
+    const winX = checkWin(X);
     
     if(winO && winX) return 0
-    if(winO > 1 && winX > 1) return 0
-    if(winO  && countO-countX !== 1) return 0
-    if(winX  && countO-countX !== 0) return 0
-    return 1
-}
-
-function getCount(board,base){
-    return board.filter(item => item === base).length
-}
-
-function checkWin(board, base){
-    let count = 0;
-    const winArr = [
-        [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
-    ]
-    winArr.forEach(win => {
-        const [a,b,c] = win
-        if(board[a] === base && board[b] === base && board[c]===base) count+=1
-    })
-    return count
+    if(winO && numO - numX !== 1) return 0
+    if(winX && numO - numX !== 0) return 0
+    
+    return 1;
 }
