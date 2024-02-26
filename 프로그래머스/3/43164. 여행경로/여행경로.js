3f5da0b8a2343e828fabdc944f50a7d7232e4da7
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-02-26 20:21:10 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,25 +1,23 @@
 function solution(tickets) {
     tickets.sort();
-    const visited = Array.from({length: tickets.length}, ()=>0);
-    let answer = [];
-    const result = [];
-    function dfs(start, layer){
-        result.push(start)
-        if(layer === tickets.length){
-            answer = result
+    const answer = [];
+    const visited = Array.from({length:tickets.length}, ()=>0);
+    function dfs(str, count){
+        answer.push(str)
+        if(count === tickets.length){
             return true;
         }
         for(let i=0; i<tickets.length; i++){
-            if(!visited[i] && tickets[i][0] === start){
-                visited[i] = 1;
-                if(dfs(tickets[i][1], layer+1)) return true;
-                visited[i] = 0;
+            if(!visited[i] && tickets[i][0] === str){
+                visited[i] = true;
+                if(dfs(tickets[i][1], count+1)) return true;
+                visited[i] = false;
             }
         }
-        result.pop();
+        answer.pop();
         return false;
     }
-    
-    dfs("ICN",0)
-    return answer
+
+    dfs("ICN",0);
+    return answer;
 }
