169a9811e4bb8be600dbca7c094fcb7fd38dea7a
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-04 12:40:18 +0900

    [level 2] Title: 땅따먹기, Time: 47.13 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,11 +1,11 @@
 function solution(land) {
-  const answer = land.reduce((acc,cur)=> {
-     return [
-         cur[0] + Math.max(acc[1], acc[2], acc[3]),
-         cur[1] + Math.max(acc[0], acc[2], acc[3]),
-         cur[2] + Math.max(acc[0], acc[1], acc[3]),
-         cur[3] + Math.max(acc[0], acc[1], acc[2]),
-     ]
-  },[0,0,0,0])
-  return Math.max(...answer)
+    const total = land.reduce((acc,cur)=>{
+        return [
+            cur[0] += Math.max(acc[1], acc[2], acc[3]),
+            cur[1] += Math.max(acc[0], acc[2], acc[3]),
+            cur[2] += Math.max(acc[0], acc[1], acc[3]),
+            cur[3] += Math.max(acc[0], acc[1], acc[2]),
+        ]
+    }, [0,0,0,0])
+    return Math.max(...total)
 }
