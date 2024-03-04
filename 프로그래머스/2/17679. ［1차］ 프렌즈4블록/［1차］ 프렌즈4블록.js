04cc3a6791f78d35b5bbb96bbea4eb80a10b881b
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-04 13:24:35 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 14.27 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,37 +1,40 @@
 function solution(m, n, board) {
-    const boards = board.map(item => item.split(''));
+    const boards = board.map(row => row.split(''));
+    const dx = [1,0,1];
+    const dy = [0,1,1];
+
     while(true){
-        const place = [];
+        
+        let possible = [];
         for(let i=0; i<m-1; i+=1){
             for(let j=0; j<n-1; j+=1){
                 const base = boards[i][j]
-                if(base === boards[i+1][j] && base === boards[i][j+1] && base === boards[i+1] [j+1] && base !== 0){
-                    place.push([i,j]);
-                }
+                if(base === boards[i+1][j] && base === boards[i][j+1] && base === boards[i+1][j+1] && base !== 0) possible.push([i,j]);
             }
         }
         
-        if(place.length === 0) break;
-        
-        place.forEach(item => {
+        if(possible.length === 0) break;
+
+        possible.forEach(item => {
             const [x,y] = item;
-            boards[x][y] = 0;
-            boards[x+1][y] = 0;
-            boards[x][y+1] = 0;
-            boards[x+1][y+1] = 0;
+            boards[x][y] = 0
+            boards[x][y+1] = 0
+            boards[x+1][y] = 0
+            boards[x+1][y+1] = 0
         })
         
-        for(let i=m-1; i>0; i-=1){
-            for(let j=0; j<n; j+=1){
+
+        for(let i=m-1; i>1; i-=1){
+            for(let j=0; j<n+1; j+=1){
                 for(let k=i-1; k>=0 && !boards[i][j]; k-=1){
-                    if(boards[k][j]){
+                    if(!boards[i][j] && boards[k][j]){
                         boards[i][j] = boards[k][j];
                         boards[k][j] = 0;
-                        break;
                     }
                 }
             }
         }
     }
-    return boards.flat().filter(item => item ===0).length
+    return boards.flat().filter(item => !item).length;
+    
 }
