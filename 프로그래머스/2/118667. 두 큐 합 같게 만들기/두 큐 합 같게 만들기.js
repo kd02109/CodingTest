65594d891a9f71d47d1d0d92dcc74bbe2bcb7990
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-05 11:38:07 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 19.59 ms, Memory: 47.5 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,29 +1,30 @@
+function getSum(arr){
+    return arr.reduce((acc, cur) => acc + cur, 0);
+}
+
 function solution(queue1, queue2) {
-    const totalArr = [...queue1, ...queue2]
-    const sum = totalArr.reduce((acc, cur)=>acc+cur)
-    let division = sum/2;
+    const arr = [...queue1, ...queue2];
+    const total = getSum(arr);
     
-    
-    if(sum%2 !== 0) return -1
-    
+    if(total % 2 !== 0) return -1
+    const mid = total / 2;
+    let queue1Sum = getSum(queue1);
     let base = 0;
-    let count = 0;
-    let answer = Infinity
-    let baseSum = queue1.reduce((acc, cur)=> acc+cur)
-    
-    if(baseSum === division) return 0
-    
-    for(let i=queue1.length; i<totalArr.length; i+=1){
-        while(baseSum > division && baseSum >0){
-            baseSum -= totalArr[base];
+    let answer = 0;
+    for(let i=queue1.length; i<arr.length; i+=1){
+        if(queue1Sum === mid) {
+            return answer;
+        }
+        while(queue1Sum > mid){
+            queue1Sum -= arr[base];
             base += 1;
-            count += 1;
+            answer += 1;
+        }        
+        if(queue1Sum === mid) {
+            return answer;
         }
-        if(baseSum < division){
-            baseSum += totalArr[i];
-            count += 1;
-        }
-        if(baseSum === division) answer = Math.min(answer, count);
+        queue1Sum += arr[i];
+        answer += 1;
     }
-    return answer === Infinity? -1 : answer;
+    return -1;
 }
