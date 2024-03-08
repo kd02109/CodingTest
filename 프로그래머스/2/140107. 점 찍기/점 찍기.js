2463a53ea56fd2cc1a327a5a86afdcfa945adefe
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-08 11:51:54 +0900

    [level 2] Title: 점 찍기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,8 @@
+function solution(k, d) {
+    let answer = 0;
+    for(let x = 0; x <= d; x+=k){
+        let y = Math.floor(Math.sqrt(d*d - x*x));
+        answer += Math.floor(y/k)+1
+    }
+    return answer;
+}
