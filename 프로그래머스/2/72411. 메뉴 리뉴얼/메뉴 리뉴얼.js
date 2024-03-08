39e760060c12d2b5ce2bf872b5402b1161d1919a
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-08 11:04:24 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 8.13 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,38 +1,36 @@
 function solution(orders, course) {
     const answer = [];
     course.forEach(count => {
-        const orderMap = new Map();
-        orders.forEach(order=>{
+        const map = new Map();
+        orders.forEach(order => {
             const visited = Array.from({length:order.length}, ()=>0);
-            dfs(visited,order,orderMap, "",count, 0)
+            dfs("", order, count, map, 0, visited);
         })
-        const mapArr = [...orderMap];
-        const max = Math.max(...mapArr.map(menu => menu[1]))
+        const countMenu = [...map];
+        const max = Math.max(...countMenu.map(item => item[1]));
         if(max > 1){
-            mapArr.forEach(menu => {
-            if(menu[1] === max) answer.push(menu[0])
+            countMenu.forEach((item) => {
+                if(item[1] === max) answer.push(item[0])
             })
         }
-
     })
     
-    function dfs(visited,order,orderMap, combo, targetCount, idx){
-        if(combo.length === targetCount){
-            const sortCombo = combo.split('').sort().join('');
-            if(!orderMap.get(sortCombo)) orderMap.set(sortCombo, 1);
-            else{
-                orderMap.set(sortCombo, orderMap.get(sortCombo)+1);
-            }
+    function dfs(menu, order, count, map, idx, visited){
+        if(order.length < count) return;
+        if(menu.length === count){
+            const re = menu.split('').sort().join('');
+            if(map.get(re)) map.set(re, map.get(re)+1);
+            else map.set(re, 1);
             return;
         }
+        
         for(let i=idx; i<order.length; i+=1){
             if(!visited[i]){
                 visited[i] = 1;
-                dfs(visited,order,orderMap, combo+order[i], targetCount, i+1)
+                dfs(menu+order[i], order, count, map, i+1, visited);
                 visited[i] = 0;
             }
         }
     }
-
-    return answer.sort();
+    return answer.sort()
 }
