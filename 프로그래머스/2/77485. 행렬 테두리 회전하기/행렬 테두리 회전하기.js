413aaddb379a6f1ccc1e54c86ccf20a0135c21d9
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-10 14:12:10 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 12.74 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -1,36 +1,40 @@
 function solution(rows, columns, queries) {
-    const map = Array.from({length: rows+2}, ()=> Array.from({length:columns+2}, ()=>0))
-    let count = 1;
+    const maps = Array.from({length:rows+1}, ()=>Array.from({length: columns+1},()=>0));
+    let idx = 1;
     for(let i=1; i<=rows; i+=1){
         for(let j=1; j<=columns; j+=1){
-            map[i][j] = count;
-            count += 1;
+            maps[i][j] = idx;
+            idx+=1;
         }
     }
     
     const answer = [];
     queries.forEach(query => {
-        const [x1,y1,x2,y2] = query;
-        let temp = map[x1][y1]
-        let minNums =  temp
-        for(let i=x1; i<x2; i+=1 ){
-            map[i][y1] = map[i+1][y1];
-            minNums = Math.min(minNums, map[i][y1])
+        let [x1,y1,x2,y2] = query;
+        let base = maps[x1][y1];
+        let min = base;
+        
+        for(let i = x1; i<x2; i+=1){
+            maps[i][y1] = maps[i+1][y1];
+            min = Math.min(min, maps[i][y1]);
         }
-        for(let i=y1; i < y2; i+=1){
-            map[x2][i] = map[x2][i+1];
-            minNums = Math.min(minNums, map[x2][i])
+        
+        for(let i=y1; i<y2; i+=1){
+            maps[x2][i] = maps[x2][i+1];
+            min = Math.min(min, maps[x2][i])
         }
-        for(let i=x2; i > x1; i-=1 ){
-            map[i][y2] = map[i-1][y2];
-            minNums = Math.min(minNums, map[i][y2])
+        
+        for(let i=x2; i>x1; i-=1){
+            maps[i][y2] = maps[i-1][y2];
+            min = Math.min(min, maps[i][y2]);
         }
-        for(let i=y2; i > y1; i-=1 ){
-            map[x1][i] = map[x1][i-1];
-            minNums = Math.min(minNums, map[x1][i])
+        
+        for(let i=y2; i>y1; i-=1){
+            maps[x1][i] = maps[x1][i-1];
+            min = Math.min(min, maps[x1][i]);
         }
-        map[x1][y1+1] = temp;
-        answer.push(minNums);
+        maps[x1][y1+1] = base;
+        answer.push(min);
     })
-    return answer;
+    return answer
 }
