bc103a7e3d15dea973cf209749a1c9c75c034520
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-14 11:18:04 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.72 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -1,46 +1,43 @@
 function solution(board) {
-    const boards = board.map(item => item.split(''));
-    let start = []
-    let end = []
+    const boards = board.map(row => row.split(''));
+    let start = [0,0]
+    
     
     for(let i=0; i<boards.length; i+=1){
         for(let j=0; j<boards[0].length; j+=1){
-            if(boards[i][j] === "R") start = [i, j];
-            if(boards[i][j] === "G") end = [i, j];
+            if(boards[i][j] === "R"){
+                start = [i,j];
+                break;
+            }
         }
     }
     
-    function BFS(start, end){
-        const dx = [1,0,-1,0];
-        const dy = [0,1,0,-1];
-        const queue = []
-        
-        queue.push([...start,0])
-        boards[start[0]][start[1]] = 1
+    function bfs(){
+        const queue = [[...start,0]];
+        const dx = [0,0,1,-1];
+        const dy = [1,-1,0,0];
+        boards[start[0]][start[1]] = 0;
         while(queue.length){
-            let [x,y, time] = queue.shift();
+            let [x,y,time] = queue.shift();
             for(let i=0; i<dx.length; i+=1){
-                let nx = x+dx[i];
-                let ny = y+dy[i];
-                while(nx>=0 && nx<board.length && ny>=0 && ny<board[0].length && boards[nx][ny] !== "D"){
+                let nx = x + dx[i]
+                let ny = y + dy[i]
+                while(nx>=0 && nx <boards.length && ny >=0 && ny < boards[0].length && boards[nx][ny] !== "D"){
                     nx += dx[i];
                     ny += dy[i];
                 }
                 nx -= dx[i];
                 ny -= dy[i];
-                
-                if(nx === end[0] && ny === end[1]){
-                    return time + 1
-                }
-                if(boards[nx][ny] !== 1){
+
+                if(boards[nx][ny] === "G") return time + 1;
+                if(boards[nx][ny] !== 0){
+                    boards[nx][ny] = 0;
                     queue.push([nx,ny,time+1])
-                    boards[nx][ny] = 1
-                }
+                } 
             }
         }
-        return -1;
+        return -1
     }
-    const answer = BFS(start, end);
-
+    const answer = bfs();
     return answer;
 }
