91d8b18a6f0b24af37cf058d20d13fcb5d7d2f1a
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-18 13:45:04 +0900

    [level 2] Title: 후보키, Time: 0.94 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.js
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -1,64 +1,47 @@
 function solution(relation) {
-    const rowLen = relation.length;
-    const colLen = relation[0].length;
-    const set = new Set();
-    const visited = Array.from({length: colLen}, ()=> 0);
+    const idx = relation[0].map((_,idx) => idx);
+    const combination = [];
     
-    // 모든 가능한 조합 구하기
-    const combi = new Set();
-    function dfs(key, idx){
-        if(key.length > colLen) return;
-        key = key.split('').sort().join('');
-        if(combi.has(key)) return;
-        combi.add(key)
-        for(let i=idx+1; i<colLen; i+=1){
-            if(!visited[i]){
-                visited[i] = 1;
-                dfs(key + i, idx);
-                visited[i] = 0;
-            }
-        }
-    }
-    dfs("", -1)
-    
-    const combiSort = [...combi].slice(1).sort((a,b) => a.length - b.length).map(item => {
-        const arr = item.split('').map(item => Number(item));
-        return arr
-    })
-    
-    // 관계키 가능 여부 저장하기
-    const candidate = [];
-    
-    function checkUnique(arr){
-        const isUnique = new Set();
-        for(let i=0; i<rowLen; i+=1){
-            let str = "";
-            for(let j=0; j<arr.length; j+=1){
-                str += relation[i][arr[j]]
-            }
-            isUnique.add(str)
-        }
-        return [...isUnique].length === rowLen;
-    }
-    
-    function isMinimality(combo, candidate){
-        let minumality = true;
-        candidate.forEach(set => {
-            let cnt = 0;
-            set = set.split('');
-            set.forEach(idx => {
-                if(combo.includes(idx)) cnt += 1;
-            })
-            if(cnt === set.length) minumality = false
+    function getCombi(arr,combi){
+        if(combi.length) combination.push(combi);
+        if(arr.length === 0) return;
+        arr.forEach((value, idx, arr) => {
+            const rest = arr.slice(idx+1);
+            getCombi(rest, [...combi , value]);
         })
-        return minumality
+    }
+    getCombi(idx, [])
+
+    const answer = [];
+    
+    function checkUnique(combi){
+        const set = new Set();
+        for(let i=0; i<relation.length; i+=1){
+            let base = ""
+            for(let j=0; j<combi.length; j+=1){
+                base += relation[i][combi[j]]
+            }
+            set.add(base);
+        }
+        return [...set].length === relation.length;
     }
     
-    for(let i=0; i<combiSort.length; i+=1){
-        let base= combiSort[i].join('')
-        if(checkUnique(combiSort[i]) && isMinimality(base, candidate)) candidate.push(base);
+    function checkMinimality(combi){
+        for(let i=0; i<answer.length; i+=1){
+            let count = 0;
+            let set = answer[i];
+            for(let j=0; j<set.length; j+=1){
+                if(combi.includes(set[j])) count += 1;
+            }
+            if(count === set.length) return false;
+        }
+        return true;
     }
+    
+   combination.sort((a,b) => a.length - b.length);
+    combination.forEach((combi)=> {
+        if(checkUnique(combi) && checkMinimality(combi)) answer.push(combi);
+    })
 
-    return candidate.length
+    return answer.length;
 }
-
