5c4a70ab4f307b61b0a7dc1adff178f0ef5519aa
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-20 13:38:47 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,15 +1,14 @@
 function solution(priorities, location) {
-    const priority = priorities.map((item, idx) => [item,idx])
-    const queue = [];
+    let answer = 0;
+    const maps = priorities.map((item, idx) => [idx,item]);
     
-    while(priority.length){
-        const [item,idx] = priority.shift();
-        if(priority.some(el => el[0] >item)){
-            priority.push([item,idx])
+    while(true){
+        const [idx, value] = maps.shift();
+        if(maps.every(item => item[1] <= value)){
+            answer += 1;
+            if(location === idx) return answer
         }else{
-            queue.push([item,idx])
+            maps.push([idx,value]);
         }
     }
-
-    return queue.findIndex(item => item[1] === location) + 1
 }
