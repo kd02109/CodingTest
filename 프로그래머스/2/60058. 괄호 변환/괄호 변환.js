230322bf69223bf3f7ac10959403eb40626f26ec
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-22 17:18:51 +0900

    [level 2] Title: 괄호 변환, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,25 +1,22 @@
 function solution(p) {
-    if(p === "") return ""
+    if(p === "") return "";
     
-    let [u,v] = ["",""]
-    let first = 0;
-    let end = 0;
+    let [open, close] = [0,0];
     for(let i=0; i<p.length; i+=1){
-        u += p[i];
-        if(p[i] === "(") first += 1;
-        if(p[i] === ")") end += 1;
-        if(first === end) break
+        if(p[i] === "(") open += 1;
+        if(p[i] === ")") close += 1;
+        if(open === close) break;
     }
-    v = p.slice(first+end)
-
-    if(u[0] === "(") return u + solution(v)
+    let u = p.slice(0,open+close);
+    let v = p.slice(open+close);
+    
+    if(u[0] === "(") return u+solution(v);
     else{
-        let newU = ""
+        let newU =""
         for(let i=1; i<u.length-1; i+=1){
-            const base = u[i]
-            newU += base === "(" ? ")" : "("
+            if(u[i] === "(") newU += ")"
+            else newU +=  "("
         }
         return `(${solution(v)})`+newU
     }
 }
-//solution("(())))((())(()")
