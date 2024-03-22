66de3c71854da7b8d611bc04f646cff8e413ca6f
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-22 16:04:06 +0900

    [level 1] Title: 달리기 경주, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,27 +1,22 @@
 function solution(players, callings) {
-     // 1. ["mumu", "soe", "poe", "kai", "mine"]
-     // 2.  ["kai", "kai", "mine", "mine"]
-    
-    const race = {}
-    const ranks = {}
-    
-    players.forEach((player, idx) => {
-        ranks[player] = idx+1;
-        race[idx+1] = player;
+    const play ={}
+    const rank = {}
+    players.forEach((player,idx) => {
+        play[player] = idx + 1;
+        rank[idx+1] = player;
+    }) 
+    callings.forEach((call)=>{
+        const curRank = play[call];
+        const nextRankPlayer = rank[curRank-1];
+        
+        play[call] -= 1;
+        play[nextRankPlayer] += 1;
+        
+        rank[curRank] = nextRankPlayer;
+        rank[curRank-1] = call;
+        
+        players[curRank-1] = nextRankPlayer;
+        players[curRank-2] = call;
     })
-    
-    for(let i=0; i<callings.length; i+=1){
-        const callPlayer = callings[i]
-        const callingRank = ranks[callPlayer] //num
-        
-        const nextPlayer = race[callingRank-1]
-        const nextRank = callingRank-1
-        
-        race[nextRank] = callPlayer;
-        ranks[nextPlayer] = callingRank;
-        
-        race[callingRank] = nextPlayer;
-        ranks[callPlayer] = nextRank
-    }
-    return Array.from({length: players.length}, (_,idx)=>race[idx+1])
+    return players
 }
