5705d89ac4887ae07ceaa1aa6548d9bf308a0e53
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-22 17:58:36 +0900

    [level 3] Title: 네트워크, Time: 0.96 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,22 +1,26 @@
 function solution(n, computers) {
+    const visited = Array.from({length: n}, () => 0);
     let answer = 0;
-    const visited = Array.from({length: n}, ()=>0);
-    const queue = [];
-    for(let i=0; i<n; i+=1){
-        if(!visited[i]){
-            visited[i] = true;
-            queue.push(i);
-            answer++;
-        }
-        while(queue.length){
-            const current = queue.shift()
-            computers[current].forEach((node,idx) => {
-                if(node === 1 && !visited[idx]){
-                    queue.push(idx);
-                    visited[idx] = true;
+
+    function bfs(node) {
+        const queue = [node];
+        while (queue.length) {
+            const current = queue.shift();
+            visited[current] = 1;
+            for (let i = 0; i < n; i++) {
+                if (computers[current][i] === 1 && !visited[i]) {
+                    queue.push(i);
                 }
-            })
+            }
         }
     }
+
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            bfs(i);
+            answer++;
+        }
+    }
+    
     return answer;
 }
