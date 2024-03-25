7bd4224050bc9293dd78d363e26a55b2beee5d51
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-25 11:44:48 +0900

    [level 2] Title: 미로 탈출, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,46 +1,44 @@
-function solution(maps) {
-    const MapL = maps.map(item => item.split(''));
-    const MapE = maps.map(item => item.split(''));
-    
-    let start = [];
-    let lever = [];
-    let final = [];
-    
-    for(let i=0; i<maps.length; i++){
-        for(let j=0; j<maps[i].length; j++){
-            if(maps[i][j] === "S") start = [i,j];
-            if(maps[i][j] === "L") lever = [i,j];
-            if(maps[i][j] === "E") final = [i,j];
-        }
-    }
-    const leverTime = findMap(start, MapL, lever);
-    const endTime = findMap(lever, MapE, final);
-    if(endTime === -1 || leverTime === -1) return -1;
-    return leverTime+endTime;
-}
-
-
-function findMap(start, map, d){
-    const dx = [1, -1, 0, 0];
-    const dy = [0, 0, 1, -1];
-    
-    const [detinationX, destinaionY] = d;
-
-    const queue = [[...start,0]];
-    map[start[0]][start[1]] = "X";
-    
-
+function findMap(start, target, map){
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
+    const queue = [[...start, 0]]
+    map[start[0]][start[1]] = "X"
     while(queue.length){
-
-        const [x,y,time] = queue.shift();
+        let [x,y,time] = queue.shift();
         for(let i=0; i<dx.length; i+=1){
-            let nx = dx[i]+x;
-            let ny = dy[i]+y;
-            if(nx <0 || nx >= map.length || ny<0 || ny>=map[0].length || map[nx][ny] === "X") continue;
-            map[nx][ny] = "X"
-            queue.push([nx,ny,time+1])
-            if(nx === detinationX && ny === destinaionY) return time+1;
+            let nx = x+dx[i];
+            let ny = y+dy[i];
+            if(nx>=0 && nx < map.length && ny>=0 && ny < map[0].length && map[nx][ny] !== "X"){
+                if(map[nx][ny] === target) return time + 1;
+                queue.push([nx,ny, time + 1]);
+                map[nx][ny] = "X"
+            }
         }
     }
+    
     return -1;
 }
+
+function solution(maps) {
+  const leverMap = maps.map(item => item.split(''));
+  const endMap = maps.map(item => item.split(''));
+  let start = [0,0]
+  let lever = [0,0]
+  
+  for(let i=0; i<maps.length; i+=1){
+      for(let j=0; j<maps[0].length; j+=1){
+          if(maps[i][j] === "S") start = [i,j];
+          if(maps[i][j] === "L") lever = [i,j]
+      }
+  }
+    
+    
+  const toLever = findMap(start, "L", leverMap);
+  const toEnd = findMap(lever, "E", endMap);
+     
+  if(toLever === -1 || toEnd === -1) return -1;
+  return toLever + toEnd;
+    
+}
+
+
