b7b28a88d90c9b3688b61897aff01cfca573f4bb
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-25 12:18:20 +0900

    [level 2] Title: 삼각 달팽이, Time: 84.23 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -1,18 +1,19 @@
 function solution(n) {
-  const dx = [1, 0, -1];
-  const dy = [0, 1, -1];
-  const map = Array.from({ length: n }, (_, idx) => new Array(idx + 1).fill(0));
-  let number = 1;
-  let dir = 0;
-  let [x, y] = [-1, 0];
-  for (let i = n; i > 0; i -= 1) {
-    for (let j = 0; j < i; j += 1) {
-      x += dx[dir];
-      y += dy[dir];
-      map[x][y] = number;
-      number += 1;
+    const dx = [1,0,-1];
+    const dy = [0,1,-1];
+    const map = Array.from({length:n}, (_,idx)=> new Array(idx+1).fill(0))
+    let num = 1;
+    let x = -1;
+    let y = 0;
+    let dir = 0;
+    for(let i = n; i>0; i-=1){
+        for(let j=0; j<i; j+=1){
+            x += dx[dir];
+            y += dy[dir];
+            map[x][y]=num;
+            num += 1;
+        }
+        dir = dir !== 2 ? dir+1 : 0
     }
-    dir = (dir + 1) % 3;
-  }
-  return map.flat();
+   return map.flat()
 }
