d77d2f9b8503d2f3a5f7db5756ac5dae76558f3d
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-03-27 10:37:41 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 13.05 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,40 +1,41 @@
 function solution(m, n, board) {
-    const boards = board.map(row => row.split(''));
-    const dx = [1,0,1];
-    const dy = [0,1,1];
-
+    board = board.map(item => item.split(''));
+    
     while(true){
-        
-        let possible = [];
+        const possible = [];
         for(let i=0; i<m-1; i+=1){
             for(let j=0; j<n-1; j+=1){
-                const base = boards[i][j]
-                if(base === boards[i+1][j] && base === boards[i][j+1] && base === boards[i+1][j+1] && base !== 0) possible.push([i,j]);
+                const base = board[i][j];
+                if(base === board[i][j+1] && base === board[i+1][j] && base === board[i+1][j+1] && base !== 0) possible.push([i,j]);
             }
         }
         
-        if(possible.length === 0) break;
-
+        // 정답 도출
+        if(!possible.length){
+            return board.flat().filter(item => item === 0).length ;
+        }
+        
+        // 겹치는 block 삭제
         possible.forEach(item => {
             const [x,y] = item;
-            boards[x][y] = 0
-            boards[x][y+1] = 0
-            boards[x+1][y] = 0
-            boards[x+1][y+1] = 0
-        })
+            board[x][y] = 0;
+            board[x+1][y] = 0;
+            board[x][y+1] = 0;
+            board[x+1][y+1] = 0;
+        });
         
-
-        for(let i=m-1; i>1; i-=1){
-            for(let j=0; j<n+1; j+=1){
-                for(let k=i-1; k>=0 && !boards[i][j]; k-=1){
-                    if(!boards[i][j] && boards[k][j]){
-                        boards[i][j] = boards[k][j];
-                        boards[k][j] = 0;
+        // board 재배열
+        // 밑에서부터 위로 바꾸기
+        for(let i=m-1; i>0; i-=1){
+            for(let j=0; j<n; j+=1){
+                for(let z = i-1; z>=0 && !board[i][j]; z-=1){
+                    if(board[z][j]){
+                        board[i][j] = board[z][j];
+                        board[z][j] = 0;
+                        break;
                     }
                 }
             }
         }
     }
-    return boards.flat().filter(item => !item).length;
-    
 }
