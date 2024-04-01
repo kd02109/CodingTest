293575dcc0cb4de1e1b867656cc1c89d22b18882
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-01 09:43:56 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,15 +1,20 @@
 function solution(storey) {
-    let answer = Infinity;
-    function dfs(floor, count){
+    let answer = 100_000_000;
+    function dfs(storeis, count){
         if(count > answer) return;
-        if(floor === 0){
-            answer = Math.min(count, answer);
-            return;
-        }
-        const rest = floor % 10;
-        dfs(Math.floor(floor/10), count+rest);
-        dfs(Math.floor(floor/10)+1, count+(10-rest));
+        
+        if(storeis === 0){
+            answer = Math.min(answer, count);
+            return
+        };
+        
+        const quad = Math.floor(storeis/10);
+        const dist = storeis % 10;
+        
+        dfs(quad, count+dist);
+        dfs(quad+1, count+(10-dist));
     }
-    dfs(storey, 0)
+    dfs(storey,0);
+    
     return answer;
 }
