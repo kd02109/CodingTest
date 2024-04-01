832176754bb45e2176964affb8cc95403e6f50cd
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-01 11:32:01 +0900

    [level 2] Title: 양궁대회, Time: 5.66 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -0,0 +1,41 @@
+function solution(n, info) {
+    
+    if(info[0] === n) return [-1];
+    
+    var answer = [];
+    const board = [0,1,2,3,4,5,6,7,8,9,10].reduce((acc,cur,idx) => {
+        acc[cur] = 10 - idx;
+        return acc
+    }, {})
+
+    function dfs(idx, count, info, score, my){
+        if(idx > 10){
+            my[my.length-1] += count;
+            answer.push([score, my]);
+            return;
+        }
+        
+        const apaci = info[idx];
+        const currentScore = board[idx];
+        
+        if(apaci){
+            dfs(idx+1, count, info, score-currentScore, [...my, 0]);
+        }else{
+            dfs(idx+1, count, info, score, [...my, 0]);
+        }
+        
+        if(count >= apaci + 1){
+            dfs(idx+1, count-(apaci+1), info, score+currentScore, [...my, apaci+1]);
+        }
+    }
+    dfs(0,n,info,0,[]);
+    answer.sort((a,b)=> {
+        if(b[0] === a[0]){
+            return b[1].filter(item => item !==0).length - a[1].filter(item => item !==0).length
+        }
+        return b[0]-a[0]
+    });
+    
+    if(answer[0][0] === 0) return [-1]
+    return answer[0][1];
+}
