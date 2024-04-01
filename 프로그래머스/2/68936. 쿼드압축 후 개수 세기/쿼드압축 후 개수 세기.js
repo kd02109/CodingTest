f8c49a066b555fda994c7dc60960bfada199b5c9
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-01 09:32:30 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 7.62 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,30 +1,31 @@
 function solution(arr) {
     const answer = [0,0];
-    function dfs(rowStart, colStart, rowEnd, colEnd){
-        const base = arr[rowStart][colStart];
-        let flag = true;
-        
-        for(let i = rowStart; i<rowEnd; i+=1){
-            for(let j = colStart; j<colEnd; j+=1){
-                if(base !== arr[i][j] ){
-                    flag = false;
+    
+    function dfs(col,row, colEnd, rowEnd){
+        const base = arr[row][col];
+        let isPossible = true;
+        for(let i=row; i<rowEnd; i+=1){
+            for(let j=col; j<colEnd; j+=1){
+                if(base !== arr[i][j]){
+                    isPossible = false;
                     break;
-                }
+                } 
             }
         }
-        
-        if(flag){
+        if(isPossible){
             answer[base] += 1;
             return;
         }
         
-        const half = (rowEnd - rowStart) / 2;
-        dfs(rowStart, colStart, rowEnd - half, colEnd - half);
-        dfs(rowStart, colStart + half, rowEnd - half, colEnd);
-        dfs(rowStart + half, colStart, rowEnd, colEnd - half);
-        dfs(rowStart + half, colStart + half, rowEnd, colEnd);
+        const colHalf = (colEnd-col)/2;
+        const rowHalf = (rowEnd-row)/2;
+        dfs(col, row, colEnd - colHalf, rowEnd - rowHalf);
+        dfs(col+colHalf, row, colEnd, rowEnd - rowHalf);
+        dfs(col, row + rowHalf, colEnd - colHalf, rowEnd);
+        dfs(col+colHalf, row+rowHalf, colEnd, rowEnd);
     }
-    dfs(0,0, arr.length, arr.length);
     
-    return answer;
+    dfs(0,0,arr.length, arr[0].length);
+    
+    return answer
 }
