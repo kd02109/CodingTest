10f5a647707e81098b887ee471377d8f104a54ec
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-02 11:14:41 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,22 +1,21 @@
-function fact(n){
-    let sum = 1;
-    for(let i=1; i<=n; i+=1){
-        sum *= i;
+function getFactorial(len){
+    let factorial = 1;
+    for(let i=1; i<=len; i+=1){
+        factorial *= i;
     }
-    return sum;
+    return factorial;
 }
 
-
 function solution(n, k) {
-    let arr = Array.from({length : n}, (_, idx)=>idx+1);
+    const num = Array.from({length:n}, (_,idx)=> idx+1);
     const answer = [];
-    let idx = k-1;
-    while(arr.length){
-        const factorial = fact(arr.length-1);
-        const q = Math.floor(idx/factorial);
-        answer.push(arr[q]);
-        arr.splice(q,1);
-        idx = idx%factorial
+    let idx = k-1
+    while(answer.length !== n){
+        const factorial = getFactorial(num.length-1);
+        const curIdx = Math.floor(idx/factorial);
+        idx = idx%factorial;
+        answer.push(num[curIdx]);
+        num.splice(curIdx, 1);
     }
     return answer;
 }
