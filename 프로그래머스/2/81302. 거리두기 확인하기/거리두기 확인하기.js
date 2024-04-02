212dcfc507bc647a1cb1e32733133b6eb220d457
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-02 10:44:51 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -1,40 +1,36 @@
 function checkPlace(place){
-    place = place.map(item => item.split(''));
-    const people = []
+    const stack = [];
+    
+    const dx = [0,0,1,-1];
+    const dy = [1,-1,0,0];
+    
     for(let i=0; i<place.length; i+=1){
         for(let j=0; j<place.length; j+=1){
-            if(place[i][j] === "P") people.push([i,j]);
+            if(place[i][j] === "P") stack.push([i,j])
         }
     }
-    const dx = [1,-1,0,0];
-    const dy = [0,0,1,-1]
-    while(people.length){
-        let [x,y] = people.pop();
+    
+    while(stack.length){
+        const [x,y] = stack.pop();
         for(let i=0; i<dx.length; i+=1){
-            let nx = x+dx[i];
-            let ny = y+dy[i];
-            if(nx>=0 && nx<5 && ny>=0 && ny<5){
-                if(place[nx][ny] === "X") continue;
-                if(place[nx][ny] === "P") return 0;
-                for(let j=0; j<dx.length; j+=1){
-                    let nnx = nx + dx[j];
-                    let nny = ny + dy[j];
-                    if(nnx>=0 && nnx<5 && nny>=0 && nny<5){
-                        if(nnx === x && nny === y) continue;
-                        if(place[nnx][nny] === "P") return 0;
-                    }
-                }
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+            if(nx<0 || nx >= place.length || ny<0 || ny >=place[0].length || place[nx][ny] === "X") continue
+            if(place[nx][ny] === "P") return 0;
+            for(let j=0; j<dx.length; j+=1){
+                let nnx = nx + dx[j];
+                let nny = ny + dy[j];
+                if(nnx<0 || nnx >= place.length || nny<0 || nny >=place[0].length || place[nnx][nny] === "X") continue;
+                if(nnx === x && nny === y) continue;
+                if(place[nnx][nny] === "P")
+                    return 0;
             }
         }
     }
+    
     return 1;
 }
 
-
-function solution(places) {
-    const answer = [];
-    places.forEach(place => {
-        answer.push(checkPlace(place))
-    })
-    return answer;
+function solution(places) {    
+    return places.map((place) => checkPlace(place))
 }
