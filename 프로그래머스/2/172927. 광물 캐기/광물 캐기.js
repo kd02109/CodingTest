bd0d55ea799a2b61f8c46baa7df9fda203245673
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-03 12:04:27 +0900

    [level 2] Title: 광물 캐기, Time: 0.27 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,39 +1,56 @@
 function solution(picks, minerals) {
-    let totalPick = picks.reduce((acc,cur) => acc + cur);
-    let [dia,iron, stone] = picks;
-    minerals = minerals.slice(0, totalPick * 5);
-    const tireds = [];
-    const obj = {diamond: 0, iron: 0, stone: 0}
+    // 미네랄 곡괭이가 캘수 있는 최대 개수로 맞추기
+    const totalPicks = picks.reduce((acc,cur)=>acc+cur, 0);
+    minerals = minerals.slice(0, totalPicks*5);
     
-    for(let i=0; i<minerals.length; i+=5){
-        const mineralSet = minerals.slice(i,i+5);
-        const set = {...obj};
-        mineralSet.forEach(mineral => {
-            set[mineral]+=1; 
-        })
-        tireds.push([
-            set.diamond * 1 + set.iron * 1 + set.stone * 1,
-            set.diamond * 5 + set.iron * 1 + set.stone * 1,
-            set.diamond * 25 + set.iron * 5 + set.stone * 1,
-        ])
+    const score = {
+        diamond: {
+            diamond:1,
+            iron:1,
+            stone:1,
+        },
+        iron : {
+            diamond:5,
+            iron:1,
+            stone:1,
+        },
+        stone : {
+            diamond:25,
+            iron:5,
+            stone:1,
+        }
     }
     
-    tireds.sort((a,b) => {
-        return a[2]-b[2]
-    })
+    // 새로운 점수 배열 만들기
+    const newScore = [];
+    for(let i=0; i<minerals.length; i+=5){
+        const slice = minerals.slice(i,i+5);
+        let [diamond, iron, stone] = [0,0,0];
+        slice.forEach(mineral => {
+            diamond += score.diamond[mineral];
+            iron += score.iron[mineral];
+            stone += score.stone[mineral];
+        })
+        newScore.push([diamond, iron, stone]);
+    }
     
+    // 점수 배열 오름차순 정렬
+    newScore.sort((a,b) => a[2]-b[2]);
+    
+    // 최소 피로도 구하기 
+    let [dia, iron, stone] = picks;
     let answer = 0;
-    while(tireds.length){
-        const mineralScore = tireds.pop();
+    while(newScore.length){
+        const tired = newScore.pop();
         if(dia){
-            dia -=1;
-            answer += mineralScore[0]
+            dia -= 1;
+            answer += tired[0];
         }else if(iron){
             iron -=1;
-            answer += mineralScore[1]
+            answer += tired[1];
         }else{
             stone -=1;
-            answer += mineralScore[2]
+            answer += tired[2];
         }
     }
     return answer
