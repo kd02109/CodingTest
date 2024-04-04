eaf36424a6fbe82d2ae2437b4d95cfa0aeba1805
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-04 18:38:37 +0900

    [level 2] Title: N-Queen, Time: 345.93 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,30 +1,27 @@
 function solution(n) {
     const board = Array.from({length: n}, ()=> -1)
     let answer = 0;
-    function isPossible(row, col, board){
+    const check = (row, col, board) => {
         for(let i=0; i<row; i+=1){
-            // 현재 col과 row 번째 배치된 queen col이 같을지 비교
             if(col === board[i]) return false;
-            if(Math.abs(i-row) === Math.abs(col-board[i])) return false
+            if(Math.abs(row-i) === Math.abs(col-board[i])) return false;
         }
-        return true;   
+        return true;
     }
     
-    function dfs(row, board){
-        const tmpBoard = [...board]
-
-        if(row === n) {
+    const dfs = (layer, arr) =>{
+        if(layer === n){
             answer += 1;
-            //console.log(tmpBoard)
         }
-        
-        for(let col=0; col<n; col+=1){
-            if(isPossible(row, col, tmpBoard)){
-                tmpBoard[row] = col;
-                dfs(row+1, tmpBoard);
-            }
+
+        for(let i=0; i<n ; i+=1){
+            if(check(layer, i, arr)){
+              arr[layer] = i;
+              dfs(layer+1, [...arr])  
+            } 
         }
     }
-    dfs(0, board);
-    return answer
+    
+    dfs(0,board)
+    return answer;
 }
