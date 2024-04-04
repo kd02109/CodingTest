5f53ed72a7997df93303faaae73f150dc28fbb05
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-04 17:11:20 +0900

    [level 2] Title: 과제 진행하기, Time: 11.32 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -1,17 +1,19 @@
 function solution(plans) {
-    const sortPlans = plans.map(([name, time, count]) => [name, changeTime(time), Number(count)]).sort((a,b)=> b[1] - a[1]);
-    const stack = [];
-    while(sortPlans.length){
-        const [name, time, count] = sortPlans.pop();
-        stack.forEach(([name, totalCount],idx,arr) => {
-            if(totalCount > time) arr[idx][1] += count;
-        })
-        stack.push([name, time+count])
+    const changeTime = (time) => {
+        const [hour, min] = time.split(":").map(Number);
+        return hour*60 + min
     }
-    return stack.sort((a,b)=> a[1]-b[1]).map(item => item[0])
+    const newPlans = plans.map(plan => [plan[0],changeTime(plan[1]), Number(plan[2])]);
+    newPlans.sort((a,b)=> b[1]-a[1]);
+    const stack = [];
+    while(newPlans.length){
+        const [name, time, duration] = newPlans.pop();
+        stack.forEach(prev => {
+            if(prev[1] > time) prev[1] += duration;
+        })
+        stack.push([name, time+duration]);
+    }
+    
+    return stack.sort((a,b)=> a[1]-b[1]).map(item => item[0]);
 }
 
-const changeTime = (time) => {
-    const [hour, minute] = time.split(":").map(item => Number(item));
-    return hour * 60 + minute;
-}
