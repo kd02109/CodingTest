c97d09294b175f4dd973626c37f9dc03a7512209
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-05 11:10:35 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,39 +1,40 @@
 function solution(users, emoticons) {
-    const answer = [0,0];
-    const percent = [10,20,30,40];
+    const percents = [10,20,30,40];
+    let [memberships, totalPrice] = [0,0]
+    const percentSet = [];
     
-    // 할인율 모든 조합 만들기 중복 가능
-    const possiblePercent = []
-    function dfs(count, possible){
-        if(count === emoticons.length){
-            possiblePercent.push(possible);
+    // 모든 가능한 % 조합 구하기
+    const dfs = (layer, set) => {
+        if(layer === emoticons.length){
+            percentSet.push(set);
             return;
         }
-        for(let i=0; i<percent.length; i+=1){
-            dfs(count+1, [...possible, percent[i]])
+        for(let i=0; i<percents.length; i+=1){
+            dfs(layer+1, [...set, percents[i]]);
         }
     }
-    dfs(0,[])
+    dfs(0, []);
     
-    possiblePercent.forEach(percents => {
-        let totalPrice = 0;
-        let totalMember = 0;
+    //가능한 최대 맴버쉽 및 가격 구하기
+    percentSet.forEach(percent => {
+        let eachPercentTotalPrice = 0;
+        let eachpercentMembership = 0;
         users.forEach(user => {
-            const [userPercent, changeMemberPrice] = user;
-            let purchase = 0;
-            emoticons.forEach((emoticon,idx)=>{
-                if(percents[idx] >= userPercent) purchase += emoticon * ((100-percents[idx])/100)
+            const [userPercent, userPrice] = user;
+            let eachUserPrice = 0;
+            percent.forEach((value, idx)=>{
+                if(userPercent <= value) {
+                    eachUserPrice += (emoticons[idx] * ((100-value)/100));
+                }
             })
-            if(purchase >= changeMemberPrice){ 
-                totalMember += 1;
-            }else{
-                totalPrice += purchase;
-            }
+            if(eachUserPrice >= userPrice) eachpercentMembership += 1;
+            else eachPercentTotalPrice += eachUserPrice;
         })
-        if(answer[0] < totalMember || (answer[0] === totalMember && answer[1] < totalPrice)){
-            answer[0] = totalMember
-            answer[1] = totalPrice
+        if(memberships < eachpercentMembership || (memberships === eachpercentMembership && totalPrice < eachPercentTotalPrice  )){
+            memberships = eachpercentMembership;
+            totalPrice = eachPercentTotalPrice;
         }
     })
-    return answer;
+    
+    return [memberships, totalPrice]
 }
