23ae05c872cd17c26499af601f3368c981b174ef
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-05 11:23:24 +0900

    [level 2] Title: 숫자 블록, Time: 104.45 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
@@ -1,22 +1,24 @@
 function solution(begin, end) {
-    var answer = [];
-    function checkdivisor(num){
-        if(num === 1) return 0;
-        const divisors = [];
-        const sqrt = Math.sqrt(num);
+    const answer = [];
+    const checkBlock = (value) => {
+        if(value === 1) return 0;
+        
+        const sqrt = Math.sqrt(value);
+        const divisor = [];
+        
         for(let i=2; i<=sqrt; i+=1){
-            if(num%i === 0){
-                divisors.push(i);
-                if(num/i <= 10 ** 7) return num/i;
+            if(value % i ===0){
+                divisor.push(i);
+                if(value/i <= 10 ** 7) return value/i;
             }
         }
-        if(divisors.length) return Math.max(...divisors)
+        if(divisor.length) return Math.max(...divisor);
         return 1;
     }
-        
-    for(let i=begin; i<=end; i+=1){
-        const divisor = checkdivisor(i);
-        answer.push(divisor);
+    
+    for(let i = begin; i<=end; i+=1){
+        answer.push(checkBlock(i));
     }
-    return answer;
+    
+    return answer
 }
