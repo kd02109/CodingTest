562ac41128d413196a5fa3848ecc8f803327fbc1
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-08 11:23:12 +0900

    [level 2] Title: 요격 시스템, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,15 +1,15 @@
 function solution(targets) {
-    targets.sort((a,b)=> b[0] - a[0]);
+    targets.sort((a,b) => b[0] - a[0]);
+    
+    let start = targets[0][0];
     let answer = 1;
-    let checkPoint = targets[0][0];
     
     for(let i=1; i<targets.length; i+=1){
-        const [start,end] = targets[i]
-        
-        if(end <= checkPoint){
-            checkPoint = start;
-            answer += 1;
-        }
+        const [nextStart, nextEnd] = targets[i];
+        if(start >= nextEnd){
+            answer +=1;
+            start = nextStart;
+        }    
     }
-    return answer;
+    return answer
 }
