d1b3eb876aa6ece98d4b5bf2427eee1bc4b3ada8
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-08 11:15:48 +0900

    [level 2] Title: 양궁대회, Time: 3.99 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,39 +1,37 @@
 function solution(n, info) {
     const answer = [];
-    const board = [0,1,2,3,4,5,6,7,8,9,10].reduce((acc,cur,idx) => {
-        acc[cur] = 10 - idx;
+    const possible = [];
+    const scoreObj = [10,9,8,7,6,5,4,3,2,1,0].reduce((acc,cur,idx)=>{
+        acc[idx] = cur;
         return acc
-    }, {})
-
-    function dfs(idx, count, info, score, my){
-        if(idx > 10){
-            my[my.length-1] += count;
-            answer.push([score, my]);
+    }, {});
+    
+    const combi = (layer, arr, count, score) => {
+        if(layer === 10){
+            arr[layer] = count;
+            possible.push([score, arr]);
             return;
         }
-        
-        const apaci = info[idx];
-        const currentScore = board[idx];
-        
-        if(apaci){
-            dfs(idx+1, count, info, score-currentScore, [...my, 0]);
-        }else{
-            dfs(idx+1, count, info, score, [...my, 0]);
+        let apachi = 0;
+        let apachiShoot = info[layer]
+        if(apachiShoot){
+            apachi = scoreObj[layer];
         }
+        const lion = apachiShoot+1;
+        combi(layer+1,[...arr,0], count, score - apachi);
         
-        if(count >= apaci + 1){
-            dfs(idx+1, count-(apaci+1), info, score+currentScore, [...my, apaci+1]);
+        if(count >=lion){
+            combi(layer+1, [...arr, lion], count-lion, score+scoreObj[layer]);
         }
     }
-    dfs(0,n,info,0,[]);
-    
-    answer.sort((a,b)=> {
+    combi(0, [], n, 0);
+    possible.sort((a,b) => {
         if(b[0] === a[0]){
-            return b[1].filter(item => item !==0).length - a[1].filter(item => item !==0).length
+            return b[1].filter(shoot => shoot>0).length - a[1].filter(shoot => shoot>0).length
         }
-        return b[0]-a[0]
-    });
-    
-    if(answer[0][0] <= 0) return [-1]
-    return answer[0][1];
+        return b[0] - a[0]
+    })
+
+    if(possible[0][0] <= 0) return [-1];
+    return possible[0][1]
 }
