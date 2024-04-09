bdc1fc0df28449052d584af2bee753c0b57c7a81
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-09 15:02:12 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.62 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,12 +1,13 @@
 function solution(friends, gifts) {
     var answer = 0;
+    // 각 친구 이름에 맞추어 인덱스 값 주기 
+    // 인덱스에 맞추어 0으로 초기화 해당 인덱스 키 값은 선물 지수 기록
     const friendMap = friends.reduce((acc, cur, idx)=> {
         acc[cur] = idx;
         acc[idx] = cur;
         return acc
     }, {})
-    
-    const giftIdx = {...friendMap};
+
     const giftMap = Array.from({length:friends.length}, ()=> new Array(friends.length).fill(0));
     
     // 준 선물 기록
@@ -24,19 +25,18 @@ function solution(friends, gifts) {
         for(let j=0; j<giftMap.length; j+=1){
             gaveTotal += giftMap[j][i]
         }
-        giftIdx[i] = giveTotal - gaveTotal;
-        giftIdx[friendMap[i]] = giveTotal - gaveTotal;
+        friendMap[i] = giveTotal - gaveTotal;
     }
     
-
+    // 각 사람별 받을 선물 수 기록하기
     for(let i=0; i<giftMap.length; i+=1){
         let nextGift = 0;
         for(let j=0; j<giftMap.length; j+=1){
             if(i===j) continue;
             const give = giftMap[i][j];
             const get = giftMap[j][i];
-            const giveGiftIdx = giftIdx[i];
-            const getGiftIdx = giftIdx[j];
+            const giveGiftIdx = friendMap[i];
+            const getGiftIdx = friendMap[j];
             if(give > get) nextGift += 1;
             else if(give === get && giveGiftIdx > getGiftIdx) nextGift += 1;
         }
