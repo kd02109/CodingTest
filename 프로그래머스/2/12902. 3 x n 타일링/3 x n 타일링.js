a9e2b6cb76e1df383f1a9ca68d190dc40742ebf7
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-17 15:10:15 +0900

    [level 2] Title: 3 x n 타일링, Time: 8.34 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.js
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.js
@@ -1,16 +1,14 @@
 function solution(n) {
     if(n%2 === 1) return 0;
-    const arr = Array.from({length:n+1}, ()=>0);
     const NUMBER = 1_000_000_007;
+    const arr = new Array(n+1).fill(0);
     arr[2] = 3;
-    
     for(let i=4; i<=n; i+=2){
         arr[i] = arr[i-2] * 3 + 2;
-        for(let j=2; j<i-2; j+=2 ){
-            arr[i] += arr[j]*2;
+        for(let j=2; j<i-2; j += 2){
+            arr[i] += (arr[j] * 2);
         }
-        arr[i] = arr[i] % NUMBER;
+        arr[i] = arr[i] % NUMBER
     }
-
     return arr[n];
-}
+}   
