af62994d60bb5902ba421a2994d4aa4eece6abbc
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-24 12:23:45 +0900

    [level 2] Title: 조이스틱, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,19 +1,20 @@
 function solution(name) {
     let answer = 0;
-    let cursor = name.length-1;
+    let rowMoving = name.length - 1;
+    const a = "A".charCodeAt()
+    const z = "Z".charCodeAt()
     
     for(let i=0; i<name.length; i+=1){
-        const char = name[i]
-        let minMove = Math.min((name.charCodeAt(i) - 'A'.charCodeAt(0)), ( 'Z'.charCodeAt(0) - name.charCodeAt(i) + 1))
-        answer += minMove;
+        const char = name[i].charCodeAt()
+        answer += Math.min(char - a, z - char + 1)
         
-        let startA = i+1
-        while(name[startA] === "A" && startA <name.length){
-            startA += 1;
+        let nextA = i+1;
+        while(name[nextA] === "A" && nextA < name.length){
+            nextA += 1;
         }
-        
-        cursor = Math.min(cursor, i*2 + name.length-startA, 2 * (name.length-startA) + i )
+        // 현재 위치까지 앞으로 갔다가 뒤로 돌기
+        // 뒤로 먼저 출발한 후에 현재 위치까지 오기 
+        rowMoving = Math.min(rowMoving, i*2 + name.length-nextA, (name.length-nextA) * 2 + i)
     }
-    
-    return answer + cursor
+    return answer + rowMoving
 }
