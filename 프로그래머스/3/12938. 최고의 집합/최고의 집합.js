4d958da99df2d8d393ba2d460ccb41955c9cae83
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-04-24 12:37:33 +0900

    [level 3] Title: 최고의 집합, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,6 +1,6 @@
 function solution(n, s) {
     if(n > s) return [-1];
-    if(n === s) return new Array(n).fill(1);
+    //if(n === s) return new Array(n).fill(1);
     const answer = [];
     while(n > 0){
         const maxNum = Math.floor(s/n);
