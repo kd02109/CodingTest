d9b6d1d37cd41d11e2215724097ca5c84d8c8cbb
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-06-17 20:37:47 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 146 ms, Memory: 48,428 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -4,11 +4,13 @@ for tc in range(1, 10+1):
     COUNT = int(input())
     lst = list(map(int, input().split()))
     cnt = 0
+    ans = 100
     while cnt < COUNT:
         lst.sort()
         lst[0] += 1
         lst[-1] -= 1
         cnt += 1
-
+        if ans > max(lst) - min(lst):
+            ans = max(lst) - min(lst)
     lst.sort()
     print(f'#{tc} {lst[-1] - lst[0]}')
