9f0095b23be0e16eb47ead7bb6b254b450a456cf
kd02109 <57277708+kd02109@users.noreply.github.com>
2024-06-27 15:34:58 +0900

    [Bronze I] Title: 색종이, Time: 240 ms, Memory: 38440 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,7 +1,7 @@
 # 새종이
 
 
-WIDTH = 100
+WIDTH = 1000
 
 N = int(input())
 lst = [list(map(int, input().split())) for _ in range(N)]
@@ -11,17 +11,16 @@ for value in lst:
     [value[0], value[1]] = [WIDTH - value[1]-value[3]+1, value[0]]
     [value[2], value[3]] = [value[0] + value[3], value[1]+value[2]]
 
-for i in range(N):
+for i in range(N-1,-1,-1):
     idx = i+1
+    cnt = 0
     [px,py,nx,ny] = lst[i]
     for x in range(px, nx):
         for y in range(py,ny):
-            board[x][y] = idx
-
-for i in range(WIDTH+1):
-    for j in range(WIDTH+1):
-        idx = board[i][j]
-        ans[idx] += 1
+            if board[x][y] == 0:
+                board[x][y] = idx
+                cnt += 1
+    ans[idx] = cnt
 
 for i in range(1, len(ans)):
     print(ans[i])
